060cd4d3bb149fdd0eee48dcb08a5c74b44469f0
Tumble <tumblegamer@gmail.com>
2020-10-23 11:29:33 +0100

    added lists and fixed drop downs

--- critterguration.user.js
+++ critterguration.user.js
@@ -100,7 +100,7 @@
 	function createDropdown(container, name, options, shouldSelect, onchange) {
 		let input = document.createElement("select");
 		input.placeholder = name;
-		input.classList.add("custom-select");
+		input.classList.add("form-select");
 		for (let option of options) {
 			let optionElement = document.createElement("option");
 			optionElement.value = option.value;
@@ -137,6 +137,67 @@
 		}
 	};
 
+	function addItemToListGroup(container, name, color = "secondary", description, footer, corner, badge, onclick, active) {
+		let listItem = document.createElement("a");
+		container.appendChild(listItem);
+		listItem.href = "#";
+		listItem.onclick = () => onclick();
+		listItem.classList.add("list-group-item", "list-group-item-action", "text-" + color);
+		if (active) listItem.classList.add("list-group-item-" + color);
+		if (name) {
+			let headerElm = document.createElement("div");
+			headerElm.classList.add(..."d-flex w-100 justify-content-between".split(" "));
+			listItem.appendChild(headerElm);
+			let nameElm = document.createElement("h5");
+			nameElm.classList.add("md-1", "w-100");
+			nameElm.innerText = name;
+			headerElm.appendChild(nameElm);
+			listItem.nameElm = nameElm;
+			if (badge) {
+				let badgeElm = document.createElement("span");
+				badgeElm.classList.add("badge", "rounded-pill", "bg-" + color);
+				badgeElm.innerText = badge;
+				listItem.badgeElm = badgeElm;
+				nameElm.prepend(badgeElm);
+			}
+			if (corner) {
+				let cornerElm = document.createElement("small");
+				cornerElm.classList.add("text-muted", "float-right");
+				cornerElm.innerText = corner;
+				nameElm.appendChild(cornerElm);
+			}
+		}
+		if (description) {
+			let descriptionElm = document.createElement("p");
+			descriptionElm.classList.add("mb-1");
+			descriptionElm.innerText = description;
+			listItem.descriptionElm = descriptionElm;
+			listItem.appendChild(descriptionElm);
+		}
+		if (footer) {
+			let footerElm = document.createElement("small");
+			footerElm.classList.add("text-muted");
+			footerElm.innerText = footer;
+			listItem.footerElm = footerElm;
+			listItem.appendChild(footerElm);
+		}
+		return listItem;
+	}
+
+	function createListGroup(container, name) {
+		let listGroup = document.createElement("div");
+		listGroup.classList.add("list-group", "list-group-flush");
+		if (name) {
+			let label = document.createElement("div");
+			label.classList.add("card-header");
+			label.innerText = name;
+			listGroup.appendChild(label);
+		}
+		listGroup.addItem = (...p) => addItemToListGroup(listGroup, ...p);
+		container.appendChild(listGroup);
+		return listGroup;
+	}
+
 
 	function createInputRow(container, name) {
 		let group = document.createElement("div");
@@ -166,12 +227,13 @@
 		if (tabContainer.children.length > 1) tabContainer.style.display = "flex";
 		if (tabContainer.children.length == 1) setTimeout(_ => activeTab(tab), 0);
 		content.createInputRow = name => createInputRow(content, name);
+		content.createListGroup = name => createListGroup(content, name);
 		setupCreationFunctions(content, true);
 		return content;
 	}
 
 	TumbleMod.onDocumentLoaded().then(alreadyLoaded => {
-		var options = {
+		let options = {
 			text: `<svg width="34" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="cog" class="svg-inline--fa fa-cog fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
 			<path fill="currentColor" d="M487.4 315.7l-42.6-24.6c4.3-23.2 4.3-47 0-70.2l42.6-24.6c4.9-2.8 7.1-8.6 5.5-14-11.1-35.6-30-67.8-54.7-94.6-3.8-4.1-10-5.1-14.8-2.3L380.8 110c-17.9-15.4-38.5-27.3-60.8-35.1V25.8c0-5.6-3.9-10.5-9.4-11.7-36.7-8.2-74.3-7.8-109.2 0-5.5 1.2-9.4 6.1-9.4 11.7V75c-22.2 7.9-42.8 19.8-60.8 35.1L88.7 85.5c-4.9-2.8-11-1.9-14.8 2.3-24.7 26.7-43.6 58.9-54.7 94.6-1.7 5.4.6 11.2 5.5 14L67.3 221c-4.3 23.2-4.3 47 0 70.2l-42.6 24.6c-4.9 2.8-7.1 8.6-5.5 14 11.1 35.6 30 67.8 54.7 94.6 3.8 4.1 10 5.1 14.8 2.3l42.6-24.6c17.9 15.4 38.5 27.3 60.8 35.1v49.2c0 5.6 3.9 10.5 9.4 11.7 36.7 8.2 74.3 7.8 109.2 0 5.5-1.2 9.4-6.1 9.4-11.7v-49.2c22.2-7.9 42.8-19.8 60.8-35.1l42.6 24.6c4.9 2.8 11 1.9 14.8-2.3 24.7-26.7 43.6-58.9 54.7-94.6 1.5-5.5-.7-11.3-5.6-14.1zM256 336c-44.1 0-80-35.9-80-80s35.9-80 80-80 80 35.9 80 80-35.9 80-80 80z"></path>
 			</svg>`,
--- test.user.js
+++ test.user.js
@@ -49,9 +49,18 @@
 			console.log("The day is", value.text);
 		});
 
+	let testList = settingContainer.createListGroup("Bob");
+	testList.addItem("Title", "warning", "Description", "Footer", "Corner", "Badge", () => {
+		console.log("This one is the test one");
+	}, true);
+	testList.addItem("Be", "success", "To be", "That is the question", "Or Not", "To", () => {
+		console.log("Shakesphere, Of course.");
+	});
+	testList.addItem("Ok Yay", "danger", "So I've made this a thing now", "Now people can make these", "Woo-Hoo", "🎈", () => {
+		console.log("But now we are here so we can do things with it, many things.");
+	});
+
 
 	let hmmContainer = Critterguration.registerSettingsMenu({ id: "hmm", name: "HMM" });
 	hmmContainer.innerText = "Welcome to Hmm World";
-
-	Critterguration.openSettings();
 })();
