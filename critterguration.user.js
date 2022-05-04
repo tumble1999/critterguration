e1b07bc8c40ac704857be11b1783c989d7d36fad
tumble <tumblegamer@gmail.com>
2022-05-04 23:03:38 +0100

    version

--- critterguration.user.js
+++ critterguration.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Critterguration
 // @namespace    https://bcmc.ga/authors/tumblegamer/
-// @version      0.3.8.38
+// @version      0.3.9.39
 // @icon         https://github.com/tumble1999/critterguration/raw/master/icon.png
 // @author       Tumble
 // @require      https://github.com/tumble1999/mod-utils/raw/master/mod-utils.js
